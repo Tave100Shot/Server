a3a07610b6e31a42aed33b27638fc8e715b41447
hyeonbin <basilla0617@naver.com>
2024-01-26 17:17:32 +0900

    feat: SecurityConfig에 compile, search 경로 수정 (#63)

--- src/main/java/com/api/TaveShot/global/config/SecurityConfig.java
+++ src/main/java/com/api/TaveShot/global/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                                         ,"/login/**"
                                         , "/api/health"
                                         , "/api/v1/search/**"
-                                        , "/api/compile/*"
+                                        , "/api/compile/**"
                                 ).permitAll()
                                 .anyRequest().authenticated());
 
